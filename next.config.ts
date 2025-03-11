e277a464a859283e99b97751ec965a7195e8c7d2
abdullahmokbl <abdullahmokbl0@gmail.com>
2025-03-11 22:39:53 +0200

    github pages deploy settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
 };
 
 export default nextConfig;
